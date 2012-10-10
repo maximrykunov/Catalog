3a7170f59d914f1f0fee5f2b4f94f46e3f7b8813
Maxim <maxim@max-VPCS13X9R.(none)>
2012-10-10 17:14:54 +0400

    fix article

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,4 +7,14 @@ class Category < ActiveRecord::Base
   def self.only_parents_for(parent_id = nil)
     scoped.where(parent_id.nil? ? 'parent_id IS NULL' : ['parent_id = ?', parent_id]).order('depth asc, name asc')
   end
+
+  def self.whole_tree
+    cat_array = []
+    Category.roots.each do |mcat|
+      Category.each_with_level(mcat.self_and_descendants) do |cat, level|
+        cat_array << ["#{'-' * cat.level} #{cat.name}", cat.id, cat.leaf? ? {} : {disabled: true}]
+      end
+    end
+    cat_array
+  end
 end
